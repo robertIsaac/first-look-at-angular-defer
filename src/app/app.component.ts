5fa4c3a385fb37cb4e6c553efe3257c6e60d4f9a
robertIsaac <dev.robert.isaac@gmail.com>
2023-10-08 14:48:14 +0300

    perf: remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { AddCommentComponent } from './add-comment/add-comment.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AddCommentComponent, RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/http-request-example/http-request-example.component.ts
+++ src/app/http-request-example/http-request-example.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { AddCommentComponent } from '../add-comment/add-comment.component';
 import { CommentsComponent } from '../comments/comments.component';
 
 @Component({
   selector: 'app-http-request-example',
   standalone: true,
-  imports: [AddCommentComponent, CommentsComponent],
+  imports: [CommentsComponent],
   templateUrl: './http-request-example.component.html',
   styleUrls: ['./http-request-example.component.scss']
 })
